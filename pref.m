86cc14d7591e934224bf14a1ccc638aaff5896f7
heyikan <hkayakoku@gmail.com>
2015-10-28 09:06:55 +0200

    #3 robotlar için mousehover eventi eklendi

--- pref.m
+++ pref.m
@@ -3,7 +3,7 @@ classdef pref
         
         %% Panel:
         panelWeight = 35 % Koordinat duzleminde X ekseninin max uzunlugu
-        panelHeight = 23 % Koordinat duzleminde Y ekseninin max uzunlugu
+        panelHeight = 22 % Koordinat duzleminde Y ekseninin max uzunlugu
         
         %% RobotSekli:
         circleRadius = 0.5 % Robot icin olusturulacak Þeklin Buyuklugu
@@ -11,24 +11,24 @@ classdef pref
         %% Gruplama:
         groupRadius = 3 % Robotlarin Grup Olusturabilmesini saglayacak kapsama alani
         gravLimit = 0.78 % Robotlarin birbirlerine yaklasarak sahip olabilecekleri maksimum cekim degeri
-        groupCount = 3  % Grup Basi Robot Sayisi
+        groupCount = 3  % Grup Basi Robot Sayisia
         
         %% Robot
-        robotMass = 2 * 1 % her bir robotun ilk baþta sahip olduðu kütlesi
-        robotGroupedMass = -0.85 % robotlar Gruplandýktan sonraki itim kütlesi
-        robotRadius = 2     % Random ekleme sýrasýnda robot için eklenmesi istenmeyen alan yarýçapý
+        robotMass = 2 * 1 % her bir robotun ilk ba?ta sahip oldu?u k?tlesi
+        robotGroupedMass = -0.85 % robotlar Grupland?ktan sonraki itim k?tlesi
+        robotRadius = 2     % Random ekleme s?ras?nda robot i?in eklenmesi istenmeyen alan yar??ap?
         %% Cisim
-        cisimMass = 2 * -0.12 % her bir cisimin ilk baþta sahip olduðu kütlesi
-        cisimRadius = 2     % Random ekleme sýrasýnda cisim için eklenmesi istenmeyen alan yarýçapý
+        cisimMass = 2 * -0.12 % her bir cisimin ilk ba?ta sahip oldu?u k?tlesi
+        cisimRadius = 2     % Random ekleme s?ras?nda cisim i?in eklenmesi istenmeyen alan yar??ap?
         %% Hedef
-        hedefMass = 3 * 1             % her bir hedefin ilk baþta sahip olduðu kütlesi
-        hedefMassNotGrouped  = -1.50  % Hedef Nesnesinin gruplanmamýþ robotlar için oluþturulan kütle deðeri
-        hedefRadius = 2               % Random ekleme sýrasýnda hedef için eklenmesi istenmeyen alan yarýçapý
-        %% Aðýrlýk Merkezi
-        merkezMass = 2 * 1 % her bir aðýrlýk merkezinin ilk baþta sahip olduðu kütlesi.
-        leaderMass = 3     % Aðýrlýk merkezi noktasý hesaplamada kullanýlan , lider robotun kütle deðeri
-        groupMemberMass = 1 % Aðýrlýk merkezi noktasý hesaplamada kullanýlan , üye robotun kütle deðeri
-        %% Çekim Sýnýrý
-        distanceLimit = 3 % Robotlarýn Cisimlere Uygulayacaðý itme kuvvetinin geçerli olduðu uzaklýk
+        hedefMass = 3 * 1             % her bir hedefin ilk ba?ta sahip oldu?u k?tlesi
+        hedefMassNotGrouped  = -1.50  % Hedef Nesnesinin gruplanmam?? robotlar i?in olu?turulan k?tle de?eri
+        hedefRadius = 2               % Random ekleme s?ras?nda hedef i?in eklenmesi istenmeyen alan yar??ap?
+        %% A??rl?k Merkezi
+        merkezMass = 2 * 1 % her bir a??rl?k merkezinin ilk ba?ta sahip oldu?u k?tlesi.
+        leaderMass = 3     % A??rl?k merkezi noktas? hesaplamada kullan?lan , lider robotun k?tle de?eri
+        groupMemberMass = 1 % A??rl?k merkezi noktas? hesaplamada kullan?lan , ?ye robotun k?tle de?eri
+        %% ?ekim S?n?r?
+        distanceLimit = 3 % Robotlar?n Cisimlere Uygulayaca?? itme kuvvetinin ge?erli oldu?u uzakl?k
     end
 end
---
+++ selectedObj.m
@@ -0,0 +1,7 @@
+classdef selectedObj
+    % Panelde o an seçili robot için tutulmasý gerekilen deðerler.
+    properties
+        circleID % Robot seçildiðinde etrafýný saran kýrmýzý circle adresini tutan deðiþken
+        robotID % seçili robota ait id
+    end
+end
